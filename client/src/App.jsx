552702fd6a7205600fe7bd9cfbff92ba29ced3f8
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-11-02 09:44:35 +0530

    completed updated listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return (
@@ -36,6 +37,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -300,9 +300,12 @@ export default function Profile() {
                 >
                   Delete
                 </button>
-                <button className="text-green-600 uppercase transition hover:text-green-700">
-                  Edit
-                </button>
+
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-600 uppercase transition hover:text-green-700">
+                    Edit
+                  </button>
+                </Link>
               </div>
             </div>
           ))}
